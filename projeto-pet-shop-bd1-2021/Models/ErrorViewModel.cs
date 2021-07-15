fecdc839101f34ac9dfbd645e0f36224f2e76f26
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-14 21:05:06 -0300

    create project

---
+++ projeto-pet-shop-bd1-2021/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projeto_pet_shop_bd1_2021.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
