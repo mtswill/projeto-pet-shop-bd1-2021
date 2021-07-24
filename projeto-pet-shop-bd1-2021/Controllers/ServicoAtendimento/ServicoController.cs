ac4c4e53403c4fd0fd7ebf83540a1672e5e3714e
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-24 15:54:30 -0300

    operacoes servico

---
+++ projeto-pet-shop-bd1-2021/Controllers/ServicoAtendimento/ServicoController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projeto_pet_shop_bd1_2021.Repositories;
+using projeto_pet_shop_bd1_2021.Models.ServicosAtendimentos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto_pet_shop_bd1_2021.Controllers.ServicoAtendimento
+{
+    public class ServicoController : Controller
+    {
+        private GenericRepository<Servico> _repository;
+
+        public ServicoController(GenericRepository<Servico> repository)
+        {
+            _repository = repository;
+        }
+
+        #region Views
+
+        public ActionResult Index()
+        {
+            var model = _repository.GetAll();
+            return View(model);
+        }
+
+        public ActionResult Details(long id)
+        {
+            var model = _repository.FindById(id);
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(long id)
+        {
+            var model = _repository.FindById(id);
+            return View(model);
+        }
+
+        public ActionResult Delete(long id)
+        {
+            var model = _repository.FindById(id);
+            return View(model);
+        }
+
+        #endregion Views
+
+        #region Manipulação de dados
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Servico servico)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.Create(servico);
+                return RedirectToAction(nameof(Details), new { id = servico.Id });
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Servico servico)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.Update(servico);
+                return RedirectToAction(nameof(Details), new { id = servico.Id });
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(long? id)
+        {
+            if (id != null)
+            {
+                _repository.Delete((long)id);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        #endregion Manipulação de dados
+    }
+}
--- projeto-pet-shop-bd1-2021/Models/ServicosAtendimentos/Servico.cs
+++ projeto-pet-shop-bd1-2021/Models/ServicosAtendimentos/Servico.cs
@@ -1,11 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace projeto_pet_shop_bd1_2021.Models.ServicosAtendimentos
 {
     public class Servico : BaseEntity
     {
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
+
+        [Display(Name = "Custo")]
         public decimal Custo { get; set; }
+
+        [Display(Name = "Valor")]
         public decimal Valor { get; set; }
     }
 }
