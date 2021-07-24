530fefdfd583bf0f32798e002d6877e3b31b495d
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-24 16:02:48 -0300

    animal views

--- projeto-pet-shop-bd1-2021/Controllers/Animal/TipoAnimalController.cs
+++ projeto-pet-shop-bd1-2021/Controllers/Animal/TipoAnimalController.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace projeto_pet_shop_bd1_2021.Controllers.Animal
+namespace projeto_pet_shop_bd1_2021.Controllers.AnimalController
 {
     public class TipoAnimalController : Controller
     {
